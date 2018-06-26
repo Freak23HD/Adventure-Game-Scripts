052efd167a2045212cd87e0a41ba679578099c38
Freak23HD <39835430+Freak23HD@users.noreply.github.com>
2018-06-26 17:41:59 +0200

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    Rigidbody2D rb;
+    public Transform rayAnchor;
+
+    public float speed;
+    public float jumpheight;
+    public float jumpDistance;
+
+    public bool moveInAir;
+
+    bool onGround;
+    // Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+	}
+
+
+    private void FixedUpdate() {
+        if (onGround || moveInAir)
+        {
+            var walkforce = Input.GetAxis("Horizontal") * speed;
+
+            rb.AddForce(transform.right * walkforce);
+        }
+        if (Input.GetButtonDown("Jump"))
+            Jump();
+
+
+        var hitInfo = Physics2D.Raycast(rayAnchor.position, -rayAnchor.up);
+        onGround = hitInfo.collider != null && hitInfo.distance < jumpDistance;
+    }
+
+    void Jump() { 
+
+         if(onGround) 
+            rb.AddForce(rayAnchor.up * jumpheight);
+    }
+
+}
---
+++ Respawn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public float minHeightForDeath;
+    public GameObject player;
+
+    void Update()
+    {
+        if (player.transform.position.y < minHeightForDeath)
+            player.transform.position = spawnPoint.position;
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Deadly")) 
+
+
+        {
+            player.transform.position = spawnPoint.position;
+        }
+
+
+       
+    }
+}
+
